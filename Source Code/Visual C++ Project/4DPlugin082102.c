25c186cf3415f4e49dfd82a11abbc7745354d931
OrchardSoftware <dwaddell@orchardsoft.com>
2014-06-27 13:31:17 -0400

    Revert "Version 6.2"
    This reverts commit d0f5b41dacd3f6e97d6d08491375d0f75a880b4c.

--- Source Code/Visual C++ Project/4DPlugin082102.c
+++ Source Code/Visual C++ Project/4DPlugin082102.c
@@ -150,8 +150,6 @@ void sys_ShellExecute( PA_PluginParameters params )
 			} while (*pChar++ != '\0');
 		}
 		
-		CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE); // AMS 5/6/14 #38727
-
 		returnValue = (LONG_PTR) ShellExecuteW(NULL, operation, file, parameters, directory, howToShow);
 		
 		strcpy(returnText, "");

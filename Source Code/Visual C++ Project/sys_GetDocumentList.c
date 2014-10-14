51b9921ad4e5ccff61883526ba1154bf96140267
amsheingold <asheingold@orchardsoft.com>
2014-10-14 09:44:58 -0400

    sys_GetDocumentList standards update
    Updated variables and formatting to Harvest standards

--- Source Code/Visual C++ Project/sys_GetDocumentList.c
+++ Source Code/Visual C++ Project/sys_GetDocumentList.c
@@ -31,72 +31,73 @@
 // ------------------------------------------------
 void sys_GetDocumentList(PA_PluginParameters params)
 {
-	LONG_PTR returnValue = 0;
-	LONG_PTR fileCount = 0;
-	LONG_PTR arraySize = 0;
-	LONG_PTR endIndex = 0;
-	LONG_PTR count = 0;
-	const LONG_PTR fileLimit = 1000;
+	LONG_PTR lReturnValue = 0;
+	LONG_PTR lFileCount = 0;
+	LONG_PTR lArraySize = 0;
+	LONG_PTR lEndIndex = 0;
+	LONG_PTR lCount = 0;
+	const LONG_PTR lFileLimit = 1000;
 	char fullPath[MAXBUF];
 	WIN32_FIND_DATA fFindData;
 	DWORD ret = 0;
 	DWORD lastError = 0;
 	HANDLE NextFind = 0;
-	BOOL getAllFiles = FALSE;
+	BOOL bGetAllFiles = FALSE;
 	WIN32_FIND_DATA fileList[1000];
 
 	// parameter variables
-	char *pathName = NULL;
-	char *filePattern = NULL;
-	LONG_PTR maxFilesToReturn = 0;
-	LONG_PTR fileSort = 0;
-	LONG_PTR startIndex = 0;
+	char *patPathName = NULL;
+	char *patFilePattern = NULL;
+	LONG_PTR palMaxFilesToReturn = 0;
+	LONG_PTR palFileSort = 0;
+	LONG_PTR palStartIndex = 0;
 	PA_Variable paReturnFileList;
 
 
 	// Get the function parameters.
-	pathName = getTextParameter(params, 1);
-	filePattern = getTextParameter(params, 2);
+	patPathName = getTextParameter(params, 1);
+	patFilePattern = getTextParameter(params, 2);
 	paReturnFileList = PA_GetVariableParameter(params, 3);
-	maxFilesToReturn = PA_GetLongParameter(params, 4);
-	fileSort = PA_GetLongParameter(params, 5);  // AMS2 9/30/14 #40405  Moved the sort parameter to be before start index
-	startIndex = PA_GetLongParameter(params, 6);  // AMS2 9/16/14 #40405
+	palMaxFilesToReturn = PA_GetLongParameter(params, 4);
+	palFileSort = PA_GetLongParameter(params, 5);  // AMS2 9/30/14 #40405  Moved the sort parameter to be before start index
+	palStartIndex = PA_GetLongParameter(params, 6);  // AMS2 9/16/14 #40405
 
 	// Clear out the return array.
 	PA_ResizeArray(&paReturnFileList, 0);
 
 	// AMS2 9/19/14 #40405 Passing in the start index is optional, if a value is passed in then it is assumed that the user put in a number relative to a starting index of 1 instead of 0 for c/c++ arrays
-	if ((startIndex != NULL) || (startIndex > 0)){
-		startIndex--;
+	if ((palStartIndex != NULL) || (palStartIndex > 0))
+	{
+		palStartIndex--;
 	}
 	else{
-		startIndex = 0;
+		palStartIndex = 0;
 	}
 
 	// AMS2 9/30/14 #40405  If they have not entered a sort option, default to the directory's sort. When adding more sort options other than by date, the method called by qsort will need to change.
-	if (fileSort == NULL)
+	if (palFileSort == NULL)
 	{
-		fileSort = 0;
+		palFileSort = 0;
 	}
 	else{
-		fileSort = 1;
+		palFileSort = 1;
 	}
 
-	if (pathName != NULL && filePattern != NULL)
+	if (patPathName != NULL && patFilePattern != NULL)
 	{
 		// Check if the path is valid.
-		ret = GetFileAttributes(pathName);
+		ret = GetFileAttributes(patPathName);
 
 		if ((ret & FILE_ATTRIBUTE_DIRECTORY) != 0 && ret != INVALID_DIRECTORY)
 		{
 			// Build the full path name.
-			if (filePattern[0] == '\0')
+			if (patFilePattern[0] == '\0')
 			{
-				snprintf(fullPath, MAXBUF, "%s%s*.*", pathName, pathName[strlen(pathName) - 1] == PATHCHAR ? "" : PATHSTR);
+				snprintf(fullPath, MAXBUF, "%s%s*.*", patPathName, patPathName[strlen(patPathName) - 1] == PATHCHAR ? "" : PATHSTR);
 			}
 			else
 			{
-				snprintf(fullPath, MAXBUF, "%s%s%s", pathName, pathName[strlen(pathName) - 1] == PATHCHAR ? "" : PATHSTR, filePattern);
+				snprintf(fullPath, MAXBUF, "%s%s%s", patPathName, patPathName[strlen(patPathName) - 1] == PATHCHAR ? "" : PATHSTR, patFilePattern);
 			}
 
 
@@ -107,15 +108,15 @@ void sys_GetDocumentList(PA_PluginParameters params)
 			{
 				// AMS2 9/18/14 #40405  Loop through the files in the directory and build a list to sort. Currently the max files that can be stored from a directory is 1000. 
 				lastError = ERROR_NO_MORE_FILES;
-				getAllFiles = (maxFilesToReturn <= 0);
+				bGetAllFiles = (palMaxFilesToReturn <= 0);
 
-				while ((NextFind != INVALID_HANDLE_VALUE) && (fileCount < fileLimit)) { 
-
+				while ((NextFind != INVALID_HANDLE_VALUE) && (lFileCount < lFileLimit)) 
+				{ 
 					if ((fFindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
 					{
 						// AMS2 9/19/14 #40405 When a file match is found, insert it into the file list array.
-						fileList[fileCount] = fFindData;
-						fileCount++;   // Count the number of files that match the pattern.
+						fileList[lFileCount] = fFindData;
+						lFileCount++;   // Count the number of files that match the pattern.
 
 					}
 					if (!FindNextFile(NextFind, &fFindData))
@@ -126,44 +127,45 @@ void sys_GetDocumentList(PA_PluginParameters params)
 				} // end while
 
 				// AMS2 9/18/14 #40405 Sort the array using compareFileTimeCreation which takes in two win32 find data variables and compares their creation dates.
-				if (fileSort == 1){
-					qsort(fileList, fileCount, sizeof(WIN32_FIND_DATA), (int(*)(const void*, const void*))compareFileTimeCreation);
+				if (palFileSort == 1)
+				{
+					qsort(fileList, lFileCount, sizeof(WIN32_FIND_DATA), (int(*)(const void*, const void*))compareFileTimeCreation);
 				}
 
 				// AMS2 9/18/14 #40405 Get all of the files if 0 is passed for the max files parameter.
-				if (maxFilesToReturn == 0)
+				if (palMaxFilesToReturn == 0)
 				{
-					maxFilesToReturn = fileCount;
+					palMaxFilesToReturn = lFileCount;
 				}
 
 				// AMS2 9/18/14 #40405 If the start index or end index is out of range, then set them to the file count.
-				if (startIndex > fileCount)
+				if (palStartIndex > lFileCount)
 				{
-					startIndex = fileCount;
+					palStartIndex = lFileCount;
 				}
 
-				endIndex = maxFilesToReturn + startIndex;
+				lEndIndex = palMaxFilesToReturn + palStartIndex;
 
-				if (endIndex > fileCount)
+				if (lEndIndex > lFileCount)
 				{
-					endIndex = fileCount;
+					lEndIndex = lFileCount;
 				}
 
 				// AMS2 9/22/14 #40405 As long as the end index is within the file limit, insert the requested file names into the return array 
 				// starting at the specified start index  and end at the start index + the max number of files to return.
-				if (endIndex <= fileLimit)
+				if (lEndIndex <= lFileLimit)
 				{
-					for (int i = startIndex; i < endIndex; i++)
+					for (int i = palStartIndex; i < lEndIndex; i++)
 					{
-						count++;
-						if (count > arraySize)
+						lCount++;
+						if (lCount > lArraySize)
 						{
 							// Increase the size of the array.
-							arraySize = count + ARRAY_LOAD_VALUE;
-							PA_ResizeArray(&paReturnFileList, arraySize);
+							lArraySize = lCount + ARRAY_LOAD_VALUE;
+							PA_ResizeArray(&paReturnFileList, lArraySize);
 						}
 						
-						PA_SetTextInArray(paReturnFileList, count, fileList[i].cFileName, strlen(fileList[i].cFileName));
+						PA_SetTextInArray(paReturnFileList, lCount, fileList[i].cFileName, strlen(fileList[i].cFileName));
 					}
 				}
 
@@ -184,8 +186,8 @@ void sys_GetDocumentList(PA_PluginParameters params)
 	if (lastError == ERROR_NO_MORE_FILES || lastError == ERROR_FILE_NOT_FOUND)
 	{
 		setError(0);
-		PA_ResizeArray(&paReturnFileList, count);  // AMS2 9/22/14 #40405 When the file selection has finished with no more files in the dir or a file has not been found, resize the return array to the number of files inserted into the array.
-		returnValue = 1;
+		PA_ResizeArray(&paReturnFileList, lCount);  // AMS2 9/22/14 #40405 When the file selection has finished with no more files in the dir or a file has not been found, resize the return array to the number of files inserted into the array.
+		lReturnValue = 1;
 	}
 	else
 	{
@@ -195,8 +197,8 @@ void sys_GetDocumentList(PA_PluginParameters params)
 
 	PA_SetVariableParameter(params, 3, paReturnFileList, 0);
 
-	freeTextParameter(pathName);
-	freeTextParameter(filePattern);
+	freeTextParameter(patPathName);
+	freeTextParameter(patFilePattern);
 
-	PA_ReturnLong(params, returnValue);
+	PA_ReturnLong(params, lReturnValue);
 }
